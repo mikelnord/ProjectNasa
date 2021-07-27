117c3000a5ab0d2d2269f510b8b28ccc23b2b83c
Mikhail <mikelnord@gmail.com>
2021-07-27 16:59:31 +1000

    commit#2

--- app/src/main/java/com/gb/projectnasa/ui/PagerFragment.kt
+++ app/src/main/java/com/gb/projectnasa/ui/PagerFragment.kt
@@ -1,10 +1,18 @@
 package com.gb.projectnasa.ui
 
+import android.graphics.Color
 import android.os.Bundle
+import android.text.Spannable
+import android.text.Spanned
+import android.text.Spanned.SPAN_INCLUSIVE_INCLUSIVE
+import android.text.style.BackgroundColorSpan
+import android.text.style.ForegroundColorSpan
+import android.text.style.RelativeSizeSpan
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
@@ -37,12 +45,45 @@ class PagerFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
 
         arguments?.let {
-            property = it.getParcelable<NasaProperty>("position")!!
+            property = it.getParcelable("position")!!
         }
         super.onViewCreated(view, savedInstanceState)
         viewModel = ViewModelProvider(this).get(PagerViewModel::class.java)
         binding.lifecycleOwner = this
         binding.nasaProperty = property
+
+        binding.mediaType.setText(property.title, TextView.BufferType.SPANNABLE)
+        val spannableText = binding.mediaType.text as Spannable
+        spannableText.setSpan(
+            ForegroundColorSpan(Color.RED),
+            0, spannableText.length,
+            SPAN_INCLUSIVE_INCLUSIVE
+        )
+        binding.data.setText(property.date, TextView.BufferType.SPANNABLE)
+        val spannableTextDate = binding.data.text as Spannable
+        spannableTextDate.setSpan(
+            BackgroundColorSpan(Color.GREEN),
+            0,
+            spannableTextDate.length,
+            Spanned.SPAN_EXCLUSIVE_EXCLUSIVE
+        )
+        binding.explanation.setText(property.explanation, TextView.BufferType.SPANNABLE)
+        val spannableTextExp = binding.explanation.text as Spannable
+        val matchResult = property.explanation?.let { str -> Regex("""Mars|Earth""").find(str) }
+        if (matchResult != null) {
+            spannableTextExp.setSpan(
+                BackgroundColorSpan(Color.GREEN),
+                matchResult.range.first,
+                matchResult.range.last,
+                Spanned.SPAN_EXCLUSIVE_EXCLUSIVE
+            )
+            spannableTextExp.setSpan(
+                RelativeSizeSpan(1.2f),
+                matchResult.range.first,
+                matchResult.range.last,
+                Spanned.SPAN_EXCLUSIVE_EXCLUSIVE
+            )
+        }
 
         val image_view = binding.imageView
         image_view.setOnClickListener {
