a34f27de9d6d85445932e59f4c330f1d168f15d5
Mikhail <mikelnord@gmail.com>
2021-07-30 09:59:11 +1000

    commit#finish

--- app/src/main/java/com/gb/projectnasa/SplashScreenActivity.kt
+++ app/src/main/java/com/gb/projectnasa/SplashScreenActivity.kt
@@ -3,6 +3,7 @@ package com.gb.projectnasa
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.coroutines.delay
 
 
 class SplashScreenActivity : AppCompatActivity() {
